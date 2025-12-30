236497fa0a3c4fbc2d2e470c873577c9da6aa7f1
AZIZABADA10 <abadaaziz288@gmail.com>
2025-12-30 14:47:19 +0000

    feat: creation du shéma du base de donnée

---
+++ DB/Shema.sql
@@ -0,0 +1,49 @@
+create database mabagnole;
+
+create table utilisateur (
+    id_utilisateur INT PRIMARY KEY AUTO_INCREMENT,
+    nom varchar(50),
+    email varchar(200),
+    `role` enum('admin', 'client') default 'client'
+);
+
+create table categorie (
+    id_categorie int PRIMARY key AUTO_INCREMENT,
+    titre: varchar(50),
+    `description` text
+);
+
+create vehicule (
+    id_vehicule INT PRIMARY KEY AUTO_INCREMENT,
+    modele varchar(40),
+    marque varchar(40),
+    prix_par_jour decimal(10,2) unsigned,
+    disponible boolean default 1,
+    `image` varchar(256),
+    id_categorie int,
+    foreign key (id_categorie) references categorie(id_categorie)
+);
+
+
+
+CREATE TABLE reservation (
+    id_utilisateur INT UNSIGNED,
+    id_vehicule INT UNSIGNED,
+    date_debut DATE,
+    date_fin DATE,
+    statut_reservation ENUM('en_attente', 'confirmee', 'annulee') DEFAULT 'en_attente',
+    PRIMARY KEY (id_utilisateur, id_vehicule),
+    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur),
+    FOREIGN KEY (id_vehicule) REFERENCES vehicule(id_vehicule)
+);
+
+
+create table avis (
+    id_utilisateur INT UNSIGNED,
+    id_vehicule INT UNSIGNED,
+    commentaire text,
+    date_avis date,
+    PRIMARY KEY (id_utilisateur,id_vehicule),
+    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur),
+    FOREIGN KEY (id_vehicule) REFERENCES vehicule(id_vehicule)
+);
