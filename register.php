2ba92054c0c12e5e413e481128a823056a27985f
AZIZABADA10 <abadaaziz288@gmail.com>
2025-12-31 00:50:12 +0000

    Correction du formulaire d'inscription avec les attributs name et gestion des erreurs

--- register.php
+++ register.php
@@ -1,3 +1,24 @@
+<?php
+session_start();
+require_once __DIR__ . '/vendor/autoload.php';
+use App\Classes\Auth;
+
+
+$errorMessage = '';
+
+if (isset($_POST['inscrire'])) {
+    $result = Auth::register($_POST['nom'],$_POST['email'],$_POST['mot_de_passe']);
+
+    if (!$result['success']) {
+        $errorMessage = $result['message'];
+    } else {
+        $errorMessage = $result['message'];
+    }
+
+}
+
+
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -16,30 +37,33 @@
             <h1 class="text-2xl font-bold text-blue-600">Créer un compte</h1>
             <p class="text-gray-500 text-sm">Rejoignez MaBagnole</p>
         </div>
+         <?php if (!empty($errorMessage)) : ?>
+            <div class="mb-4 p-3 text-red-700 bg-red-100 border border-red-400 rounded">
+                <?php echo htmlspecialchars($errorMessage); ?>
+            </div>
+        <?php endif; ?>
 
-        <form class="space-y-5">
+        <form class="space-y-5" method="POST" action="register.php">
             <div>
-                <label class="text-sm font-medium text-gray-600">Nom complet</label>
-                <input type="text" placeholder="Votre nom"
+                <label  class="text-sm font-medium text-gray-600">Nom complet</label>
+                <input name ="nom" type="text" placeholder="Votre nom"
                     class="w-full mt-1 px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-600 outline-none">
             </div>
 
             <div>
-                <label class="text-sm font-medium text-gray-600">Email</label>
-                <input type="email" placeholder="exemple@email.com"
+                <label   class="text-sm font-medium text-gray-600">Email</label>
+                <input type="email" name="email" placeholder="exemple@email.com"
                     class="w-full mt-1 px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-600 outline-none">
             </div>
 
             <div>
-                <label class="text-sm font-medium text-gray-600">Type de compte</label>
-                <select
+                <label  class="text-sm font-medium text-gray-600">Mot de passe</label>
+                <input name="mot_de_passe" type="password" placeholder="***********"
                     class="w-full mt-1 px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-600 outline-none">
-                    <option value="client">Client</option>
-                    <option value="admin">Admin</option>
-                </select>
+            
             </div>
 
-            <button
+            <button name="inscrire"
                 class="w-full bg-blue-600 text-white py-2 rounded-lg font-semibold hover:bg-blue-700 transition">
                 S'inscrire
             </button>
